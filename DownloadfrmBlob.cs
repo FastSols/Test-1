a496326e563148b13c225e68b26e588ba592aa1b
ChinmayPAt <40643158+ChinmayPAt@users.noreply.github.com>
2019-05-29 13:59:35 +0530

    Add files via upload

---
+++ DownloadfrmBlob.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+
+
+namespace App4.Activities
+{
+    [Activity(Label = "DownloadfrmBlob")]
+    public class DownloadfrmBlob : Activity
+    {
+        public static void DownloadFileAsync(String BlobPath,String DevicePath)
+        {
+            //FileStream stream = new FileStream(Path, FileMode.Open);
+            var storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials("clarify", "1uKpJnx1bKx+GWDsv+ZPbilsFxOO6lqd21XzyQixb2uGeHNPTHb1w1TaxKUlcMVDkAKezgj0Bb9Hb4+SHYb6Mg=="), true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("answers");
+
+            var blob = container.GetBlobReference(BlobPath);
+            blob.DownloadToFileAsync(DevicePath,FileMode.CreateNew);
+        }
+}
---
+++ Upload2Blob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace App4.Activities
+{
+    public class Upload2Blob
+    {
+        public static void UploadFileAsync(String Path)
+        {
+            //FileStream stream = new FileStream(Path, FileMode.Open);
+            var storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials("clarify", "1uKpJnx1bKx+GWDsv+ZPbilsFxOO6lqd21XzyQixb2uGeHNPTHb1w1TaxKUlcMVDkAKezgj0Bb9Hb4+SHYb6Mg=="),true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("answers");
+            container.CreateIfNotExistsAsync();
+            container.SetPermissionsAsync(new BlobContainerPermissions()
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+            var blob = container.GetBlockBlobReference(Path);
+            blob.UploadFromFileAsync(Path);
+            
+        }
+    }
+}
